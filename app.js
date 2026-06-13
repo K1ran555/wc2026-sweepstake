6ee7b3fd116cc830404aa4194ce07250021940b6
K1ran555 <69652367+K1ran555@users.noreply.github.com>
2026-06-13 22:34:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ var currentTab = 'leaderboard';
 var adminUnlocked = false;
 var saveTimers = {};
 var adminSubTab = 'scores';
-var scoreSubTab = 'finished';
+var scoreSubTab = 'upcoming';
 
 var MONTH_MAP = {Jan:0,Feb:1,Mar:2,Apr:3,May:4,Jun:5,Jul:6,Aug:7,Sep:8,Oct:9,Nov:10,Dec:11};
 var TOURNAMENT_YEAR = 2026;
